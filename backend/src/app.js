25e596a5f0eae172571a32e077c9ddfd0a63e1dd
Caio Damasceno <caio.damasceno@aluno.ufop.edu.br>
2023-01-13 13:18:27 -0300

    Refatorado a conexão com o MongoDb e Test dessa conexão; Test PASS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-
+const mongooseConnection = require("./config/mongooseConnection.config");
 const app = express();
+
+const index = require("./routes/index");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: "application/vnd.api+json" }));
 app.use(morgan("dev"));
 app.use(cors());
+
+app.use(index);
 
 module.exports = app;
