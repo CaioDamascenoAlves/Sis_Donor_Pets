2f1695949a000b347df7f984e8070959ac3c8532
Caio Damasceno <caio.damasceno@aluno.ufop.edu.br>
2023-01-13 13:35:18 -0300

    Server.teste commentado

--- backend/src/test/server.test.js
+++ backend/src/test/server.test.js
@@ -1,18 +1,20 @@
-const app = require('../app');
-const httpMock = require('node-mocks-http');
 const server = require('../../server');
 
+// Definindo um conjunto de testes para a configuração do servidor
 describe('Server Configuration', () => {
     let serverInstance;
 
+    //Inicia o servidor antes de todos os testes
     beforeAll(() => {
         serverInstance = server.listen(process.env.PORT || 3000);
     });
-
+    
+    //Para o servidor antes de todos os testes
     afterAll(() => {
         server.close();
     });
 
+    //Teste se o servidor está escutando na porta correta
     it('should start the server on the correct port', () => {
         expect(server.listening).toBe(true);
     });
