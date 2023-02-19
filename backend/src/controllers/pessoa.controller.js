c90517bb46bacb117afe8112c3cb52fdc3fd238e
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-19 14:13:00 -0300

    Create Pessoa apenas pra um usuario logado

--- backend/src/controllers/pessoa.controller.js
+++ backend/src/controllers/pessoa.controller.js
@@ -3,9 +3,11 @@ const Pessoa = require("../model/pessoa.model");
 exports.cratePessoa = async (req, res) => {
   try {
     // verifica se o usuário já criou uma pessoa
-    const pessoaExistente = await Pessoa.findOne({ criado_por: req.userData._id });
+    const pessoaExistente = await Pessoa.findOne({
+      criado_por: req.userData._id,
+    });
     if (pessoaExistente) {
-      return res.status(400).json({ message: 'Você já criou uma pessoa' });
+      return res.status(400).json({ message: "Você já criou uma pessoa" });
     }
 
     // cria a pessoa
@@ -25,6 +27,50 @@ exports.cratePessoa = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       message: "Ocorreu um erro ao criar a Pessoa",
+      error,
+    });
+  }
+};
+
+exports.getPessoa = async (req, res) => {
+  try {
+    const pessoa = await Pessoa.findOne({
+      id: req.params._id,
+      user: req.userData._id,
+    });
+    if (!pessoa) {
+      return res.status(404).json({ message: "Pessoa não encontrada" });
+    }
+    return res.status(200).json({ pessoa });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Ocorreu um erro ao buscar a Pessoa",
+      error,
+    });
+  }
+};
+
+exports.updatePessoa = async (req, res) => {
+  try {
+    const pessoa = await Pessoa.findOneAndUpdate(
+      { id: req.params._id, user: req.userData._id },
+      {
+        rua: req.body.rua,
+        numero: req.body.numero,
+        complemento: req.body.complemento,
+        documento: req.body.documento,
+      },
+      { new: true }
+    );
+    if (!pessoa) {
+      return res.status(404).json({ message: "Pessoa não encontrada" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Pessoa atualizada com sucesso", pessoa });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Ocorreu um erro ao atualizar a Pessoa",
       error,
     });
   }
