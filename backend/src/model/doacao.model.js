6d51f53f4958d4f1b96c7590b4a8c5db00538425
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-19 15:56:46 -0300

    Campos requeridos

--- backend/src/model/doacao.model.js
+++ backend/src/model/doacao.model.js
@@ -4,17 +4,24 @@ const { Schema } = mongoose;
 
 const doacaoSchema = new Schema(
   {
+    data: {
+      type: Date,
+      required: true,
+    },
     pet: {
       type: Schema.Types.ObjectId,
       ref: "Pet",
+      required: true,
     },
     user: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
     pessoa: {
       type: Schema.Types.ObjectId,
       ref: "Pessoa",
+      required: true,
     },
   },
   {
