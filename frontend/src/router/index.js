1444850ec2913ecba6bd52a5b8610a69655d2ebf
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-24 17:32:44 -0300

    Criando rotas iniciais

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,66 +1,64 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import NProgress from 'nprogress';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NProgress from "nprogress";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "login",
+    component: () => import("../components/auth/Login/LoginComponent"),
   },
   {
-    path: '/profile',
-    name: 'profile',
-    component: () => import('../components/auth/Profile/ProfileComponent'),
+    path: "/profile",
+    name: "profile",
+    component: () => import("../components/auth/Profile/ProfileComponent"),
     meta: {
       requireAuth: true,
     },
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('../components/auth/Login/LoginComponent'),
-    meta: {
-      requireAuth: true,
-    },
+    path: "/register",
+    name: "register",
+    component: () => import("../components/auth/Register/RegisterComponent"),
   },
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
 // Lógica inerente ao NProgress
 router.beforeResolve((to, from, next) => {
-	// Se caso não for uma página inicial de carregamento
-	if (to.name) {
-	  // Quando houver carregamento de uma página inicial, então usar o NProgress:
-	  NProgress.start();
-	}
-	next();
-  });
-  
-  // Lógica inerente ao realizar o 'Log out' remover o token no local Storage:
-  router.beforeEach((to, from, next) => {
-	if (to.matched.some((record) => record.meta.requireAuth)) {
-	  if (localStorage.getItem('jwt') == null) {
-		next({
-		  path: '/',
-		});
-	  } else {
-		next();
-	  }
-	} else {
-	  next();
-	}
-  });
-  
-  router.afterEach((to, from) => {
-	// Completando a animação da rota no NProgress
-	NProgress.done();
-  });
+  // Se caso não for uma página inicial de carregamento
+  if (to.name) {
+    // Quando houver carregamento de uma página inicial, então usar o NProgress:
+    NProgress.start();
+  }
+  next();
+});
 
-export default router
+// Lógica inerente ao realizar o 'Log out' remover o token no local Storage:
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requireAuth)) {
+    if (localStorage.getItem("jwt") == null) {
+      next({
+        path: "/",
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+router.afterEach((to, from) => {
+  // Completando a animação da rota no NProgress
+  NProgress.done();
+});
+
+export default router;
