8fc348bbbeb637d294d1791dcb7dedd8c1f4b592
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-24 17:30:29 -0300

    Consumindo dependencias

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate';
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
+import '../node_modules/nprogress/nprogress.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
 
 new Vue({
   router,
