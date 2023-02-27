f67a67ad777c07370bcc1181450d8e0423fde135
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-27 20:21:19 -0300

    Configuração de devtools

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuelidate from 'vuelidate';
 import router from './router'
+import './assets/icons';
 import 'bootstrap/dist/css/bootstrap.css'
 import '../node_modules/nprogress/nprogress.css';
 
 Vue.config.productionTip = false;
+Vue.config.devtools = true;
+Vue.config.debug = true;
+Vue.config.silent = false;
+
 
 Vue.use(Vuelidate);
 
