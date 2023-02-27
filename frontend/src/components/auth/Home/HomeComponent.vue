b3cf4d3febede35d8d7f2eb95226c1cfaf6453c4
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-27 20:22:11 -0300

    Criando pagina e logica home

---
+++ frontend/src/components/auth/Home/HomeComponent.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 offset-1">
+				<div class="card">
+					<img src="https://www.tiendanimal.pt/blog/wp-content/uploads/2022/10/como-adotar-um-gato-1200x800.jpg" class="card-img-top" alt="Imagem 1">
+					<div class="card-body">
+						<h5 class="card-title">Luna, Gata</h5>
+						<a href="link-da-imagem-1" class="btn btn-primary">Adotar</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 offset-1">
+				<div class="card">
+					<img src="https://veterinario.pt/wp-content/uploads/2015/12/cachorrinho.jpg" class="card-img-top" alt="Imagem 2">
+					<div class="card-body">
+						<h5 class="card-title">Bili, Cão</h5>
+						<a href="link-da-imagem-2" class="btn btn-primary">Adotar</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 offset-1">
+				<div class="card">
+					<img src="https://petindica.com.br/wp-content/uploads/2020/08/6-curiosidades-sobre-calopsita-que-voce-precisa-conhecer.jpg" class="card-img-top" alt="Imagem 3">
+					<div class="card-body">
+						<h5 class="card-title">Lili, Calopsita</h5>
+						<a href="link-da-imagem-3" class="btn btn-primary">Adotar</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
