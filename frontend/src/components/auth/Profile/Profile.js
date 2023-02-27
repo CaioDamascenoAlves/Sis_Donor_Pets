c8777f5928cfafaa2544e4d11a1727f50f8e15ae
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-27 20:24:37 -0300

    Pagina e hooks para exibição dos dados cadastrados pelo user JWT

--- frontend/src/components/auth/Profile/Profile.js
+++ frontend/src/components/auth/Profile/Profile.js
@@ -1,24 +1,60 @@
-import VueJwtDecode from 'vue-jwt-decode';
+import VueJwtDecode from "vue-jwt-decode";
+import PessoaService from "@/services/PessoaService";
+import PetService from "@/services/PetService";
 
 export default {
-  name: 'HomeComponent',
+  name: "ProfileComponent.vue",
   data() {
     return {
       user: {},
+      pessoa: {},
+	  pet: {},
     };
   },
   methods: {
     getUser() {
-      const token = localStorage.getItem('jwt');
+      const token = localStorage.getItem("jwt");
       const tokenDecoded = VueJwtDecode.decode(token);
       this.user = tokenDecoded;
     },
-    logOutUser() {
-      localStorage.removeItem('jwt');
-      this.$router.push('/');
+    async getPessoa() {
+      try {
+        const response = await PessoaService.GetPessoa();
+        this.pessoa = response.pessoa;
+      } catch (error) {
+        console.error(error);
+        swal({
+          title: "Oops!",
+          text: "Não foi possível carregar os dados da pessoa.",
+          icon: "error",
+        });
+      }
     },
+    async getPet() {
+      try {
+        const response = await PetService.GetPet();
+        this.pet = response.pet;
+      } catch (error) {
+        console.error(error);
+        swal({
+          title: "Oops!",
+          text: "Não foi possível carregar os dados do Pet.",
+          icon: "error",
+        });
+      }
+    },
+
+	navigateToUpdatePessoa() {
+		this.$router.push("/updatePessoa");
+	},
+	
   },
+
   created() {
     this.getUser();
+  },
+  mounted() {
+    this.getPessoa();
+	this.getPet();
   },
 };
--- frontend/src/components/auth/Profile/ProfileComponent.vue
+++ frontend/src/components/auth/Profile/ProfileComponent.vue
@@ -1,31 +1,43 @@
 <template>
-	<div>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<div class="container">
-				<a class="navbar-brand">JWT + Node + Vue</a>
-
-				<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							<a class="nav-link" @click="logOutUser">Log Out</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		<section>
-			<div class="container mt-5">
+	<div class="container col-9 offset-2 my-5">
+		<div class="card">
+			<div class="card-body">
+				<h2 class="card-title">Perfil</h2>
+				<hr>
 				<div class="row">
-					<div class="col-md-12">
-						<ul class="list-group">
-							<li class="list-group-item">Name: {{ user.name }}</li>
-							<li class="list-group-item">E-mail: {{ user.email }}</li>
-						</ul>
+					<div class="col-md-5">
+						<h4 class="card-subtitle mb-2 text-muted">Dados do Usuário</h4>
+						<p class="card-text"><strong>Nome:</strong> {{ user.name }}</p>
+						<p class="card-text"><strong>E-mail:</strong> {{ user.email }}</p>
+					</div>
+					<div class="col-md-5">
+						<h4 class="card-subtitle mb-2 text-muted">Dados da Pessoa
+							<button class="btn btn-outline-warning btn-sm mx-5" @click="navigateToUpdatePessoa">
+								<font-awesome-icon :icon="['fas', 'user-edit']" />
+								Editar
+							</button>
+						</h4>
+						<p class="card-text"><strong>Rua:</strong> {{ pessoa.rua }}</p>
+						<p class="card-text"><strong>Número:</strong> {{ pessoa.numero }}</p>
+						<p class="card-text"><strong>Complemento:</strong> {{ pessoa.complemento }}</p>
+						<p class="card-text"><strong>Documento:</strong> {{ pessoa.documento }}</p>
+					</div>
+					<div class="col-md-5 my-5">
+						<h4 class="card-subtitle mb-2 text-muted">Dados do Pet Doado
+							<button class="btn btn-outline-success btn-sm mx-5" >
+								<font-awesome-icon :icon="['fas', 'paw']" /> Editar</button>
+						</h4>
+						<p class="card-text"><strong>Nome:</strong> {{ pet.nome }}</p>
+						<p class="card-text"><strong>Espécie:</strong> {{ pet.tipo }}</p>
+						<p class="card-text"><strong>Raça:</strong> {{ pet.raca }}</p>
+						<p class="card-text"><strong>Idade:</strong> {{ pet.idade }}</p>
 					</div>
 				</div>
 			</div>
-		</section>
+		</div>
 	</div>
 </template>
-
-<script src="./Profile.js"></script>
+  
+  
+<script src="./Profile"></script>
+  
