d6bc15df5877e12181eae431418ad207c93ff421
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-28 13:56:30 -0300

    Adicionando cartão para exibição dos dados de Doação na Pagina

--- frontend/src/components/auth/Profile/ProfileComponent.vue
+++ frontend/src/components/auth/Profile/ProfileComponent.vue
@@ -24,14 +24,23 @@
 					</div>
 					<div class="col-md-5 my-5">
 						<h4 class="card-subtitle mb-2 text-muted">Dados do Pet Doado
-							<button class="btn btn-outline-success btn-sm mx-5" >
+							<button class="btn btn-outline-success btn-sm mx-5" @click="navigateToUpdatePet">
 								<font-awesome-icon :icon="['fas', 'paw']" /> Editar</button>
 						</h4>
 						<p class="card-text"><strong>Nome:</strong> {{ pet.nome }}</p>
 						<p class="card-text"><strong>Espécie:</strong> {{ pet.tipo }}</p>
 						<p class="card-text"><strong>Raça:</strong> {{ pet.raca }}</p>
 						<p class="card-text"><strong>Idade:</strong> {{ pet.idade }}</p>
+
 					</div>
+					<div class="col-md-5 my-5">
+						<h4 class="card-subtitle mb-2 text-muted">Doação
+							<button class="btn btn-outline-primary btn-sm mx-5" @click="navigateToUpdateDoacao">
+								<font-awesome-icon :icon="['fas', 'calendar']" /> Editar</button>
+						</h4>
+						<p class="card-text"><strong>Data:</strong> {{ doacao }}</p>
+					</div>
+
 				</div>
 			</div>
 		</div>
