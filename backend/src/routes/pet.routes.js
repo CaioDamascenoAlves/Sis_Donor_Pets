6a5e0441fc984b8e12ecf0155f9385242e13e619
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-03-07 08:39:21 -0300

    rotas de consulta cacheada

--- backend/src/routes/pet.routes.js
+++ backend/src/routes/pet.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const petController = require("../controllers/pet.controller");
 const auth = require("../middlewares/user.auth");
+const cache = require("../middlewares/cache");
 
 // ==> Rota responsável por Criar um novo 'Pet': (POST): localhost:3000/api/pet
 router.post("/pet", auth, petController.createPet);
@@ -11,5 +12,7 @@ router.get("/pet", auth, petController.getPet);
 
 // ==> Rota responsável por Editar 'Pet' do usuario logado: (PUT): localhost:3000/api/pet
 router.put("/pet", auth, petController.updatePet);
+
+router.get("/petcached", auth, cache.cache, petController.getPetC);
 
 module.exports = router;
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -9,8 +9,14 @@ router.post('/register',  userController.registerNewUser);
 // ==> Rota responsável por Logar o 'User': (POST): localhost:3000/api/login
 router.post('/login',  userController.loginUser);
 
+// ==> Rota responsável por Criar um 'User' no Redis: (GET): localhost:3000/api/loginR
+router.post('/loginR',  userController.loginUserR);
+
 // ==> Rota responsável por Criar um novo 'User': (GET): localhost:3000/api/register
 router.get('/userProfile', auth, userController.returnUserProfile);
+
+// ==> Rota responsável por Buscar um 'User' no Redis: (GET): localhost:3000/api/userR
+router.get('/userR', auth, userController.getUserR);
 
 // ==> Rota responsável por Criar um novo 'User': (GET): localhost:3000/api/register
 router.delete('/deleteUser', auth, userController.deleteUser);
