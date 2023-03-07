3b778883725428e9cf045ca9a7aaf577307b3d36
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-03-07 20:01:53 -0300

    Icane de coração

--- frontend/src/assets/icons.js
+++ frontend/src/assets/icons.js
@@ -1,7 +1,7 @@
 
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserPlus, faUserEdit, faTrash, faFileCirclePlus, faFilePen, faPaw, faHouse, faCalendar, faUser, faRightFromBracket} from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus, faUserEdit, faTrash, faFileCirclePlus, faFilePen, faPaw, faHouse, faCalendar, faUser, faRightFromBracket, faHeart} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-library.add(faUserPlus, faUserEdit, faTrash, faFileCirclePlus, faFilePen, faPaw, faHouse, faCalendar, faUser, faRightFromBracket);
+library.add(faUserPlus, faUserEdit, faTrash, faFileCirclePlus, faFilePen, faPaw, faHouse, faCalendar, faUser, faRightFromBracket, faHeart);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
