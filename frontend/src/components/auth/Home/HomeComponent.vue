a46a8fc0f5ab1af8f5dfcb39a086e57c8c6b993f
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-03-07 20:02:26 -0300

    Cards reponsivos

--- frontend/src/components/auth/Home/HomeComponent.vue
+++ frontend/src/components/auth/Home/HomeComponent.vue
@@ -1,38 +1,56 @@
 <template>
 	<div class="container">
-		<div class="row">
-			<div class="col-md-3 offset-1">
-				<div class="card">
-					<img src="https://www.tiendanimal.pt/blog/wp-content/uploads/2022/10/como-adotar-um-gato-1200x800.jpg" class="card-img-top" alt="Imagem 1">
-					<div class="card-body">
-						<h5 class="card-title">Luna, Gata</h5>
-						<a href="link-da-imagem-1" class="btn btn-primary">Adotar</a>
-					</div>
-				</div>
+	  <h1 class="text-center mb-5">Lista de Pets</h1>
+	  <div class="row">
+		<div class="col-md-4 mb-4" v-for="pet in pets" :key="pet._id">
+		  <div class="card shadow-sm">
+			<img :src="pet.imagemUrl" class="card-img-top" alt="Imagem do Pet">
+			<div class="card-body">
+			  <h5 class="card-title">{{ pet.nome }}</h5>
+			  <p class="card-text">Idade: {{ pet.idade }}</p>
+			  <p class="card-text">Tipo: {{ pet.tipo }}</p>
+			  <p class="card-text">Raça: {{ pet.raca }}</p>
+			  <button class="btn btn-primary " @click="logOutUser()">
+						<font-awesome-icon :icon="['fas', 'heart']" />
+						Adotar
+					</button>
 			</div>
-			<div class="col-md-3 offset-1">
-				<div class="card">
-					<img src="https://veterinario.pt/wp-content/uploads/2015/12/cachorrinho.jpg" class="card-img-top" alt="Imagem 2">
-					<div class="card-body">
-						<p class="card-text"><strong>Nome:</strong> {{ pet.nome }}</p>
-						<p class="card-text"><strong>Espécie:</strong> {{ pet.tipo }}</p>
-						<p class="card-text"><strong>Raça:</strong> {{ pet.raca }}</p>
-						<p class="card-text"><strong>Idade:</strong> {{ pet.idade }}</p>
-						<a href="link-da-imagem-2" class="btn btn-primary">Adotar</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-3 offset-1">
-				<div class="card">
-					<img src="https://petindica.com.br/wp-content/uploads/2020/08/6-curiosidades-sobre-calopsita-que-voce-precisa-conhecer.jpg" class="card-img-top" alt="Imagem 3">
-					<div class="card-body">
-						<h5 class="card-title">Lili, Calopsita</h5>
-						<a href="link-da-imagem-3" class="btn btn-primary">Adotar</a>
-					</div>
-				</div>
-			</div>
+		  </div>
 		</div>
+	  </div>
 	</div>
-</template>
+  </template>
 
 <script src="./Home.js" ></script>
+
+<style>
+.card {
+  border: none;
+  background-color: #f8f9fa;
+  border-radius: 1rem;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.card-img-top {
+  height: 200px;
+  object-fit: cover;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+}
+
+.card-title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.card-text {
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+}
+</style>
